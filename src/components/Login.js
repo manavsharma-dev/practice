94ce6300e76df04da0769b297056001cf8205839
ManavSharma03 <manav.appknit@gmail.com>
2020-06-03 15:21:20 +0530

    sign up

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,24 +15,22 @@ export default class Login extends React.Component{
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleRoute = this.handleRoute.bind(this);
   }
 
 handleChange(){
-
+  
   this.setState({
     email:this.userName.value,
-    password:this.userPass.value,
-    isSubmitted:true
+    password:this.userPass.value
   })
+
 }
 
 
 
 handleSubmit(event){
   event.preventDefault();
-  this.setState({
-    isSubmitted:true
-  })
   console.log(this.state);
   console.log(this.state.email);
 }
@@ -54,19 +52,19 @@ handleSubmit(event){
           <div className='username' name='username'>
             <label>Enter Username</label>
             <br/>
-            <input ref={userName => this.userName = userName} type='text' email={this.state.email} onChange={this.handleChange} placeholder ='Username'/>
+            <input ref={userName => this.userName = userName} type='text' onChange={this.handleChange} placeholder ='Username'/>
             <br/>
            </div>
         <div className='password' name='password'>
             <label>Enter Password</label>
             <br/>
-            <input ref={userPass => this.userPass = userPass} type='text' password={this.state.password} onChange={this.handleChange} placeholder ='Password'/>
+            <input ref={userPass => this.userPass = userPass} type='text' onChange={this.handleChange} placeholder ='Password'/>
             <br/>
               <input type='submit' value='Submit'/>
           </div>
         </form>
-      { this.state.isSubmitted === true && <Workspace username={this.state.email}/>}
-      <br/>
+      {/* { this.state.isSubmitted === true && <Workspace username={this.state.email}/>} */}
+      
       <Link to ='/'>Welcome</Link>
       </div>
     );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,16 +4,47 @@ import { Link } from 'react-router-dom'
 
 export default class Signup extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            name:'',
+            age:'',
+            email:'',
+            password:''
+        }
+
+        this.handle_Change = this.handle_Change.bind(this);
+        this.handle_Submit = this.handle_Submit.bind(this);
+
+    }
+
+    handle_Change(){
+        this.setState({
+            name:this.userName.value,
+            age:this.age.value,
+            email:this.email.value,
+            password:this.userPass.value
+        });
+    }
+
+
+    handle_Submit(event){
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+
+
     render(){
-        console.log("signup");
+
       return(
         <div className='signup'>
           <h1>Sign Up </h1>
-          <form>
+          <form onSubmit={this.handle_Submit}>
             <div className='username' name='username'>
               <label>Enter Username</label>
               <br/>
-              <input ref={userName => this.userName = userName} type='text' placeholder ='Username'/>
+              <input ref={userName => this.userName = userName} type='text' onChange = { this.handle_Change } placeholder ='Username'/>
               <br/>
             </div>
 
@@ -21,21 +52,21 @@ export default class Signup extends React.Component{
             <div className='age' name='age'>
               <label>Enter Age</label>
               <br/>
-              <input ref={userName => this.userName = userName} type='number' placeholder ='Age'/>
+              <input ref={age => this.age = age} type='number'  onChange = { this.handle_Change }  placeholder ='Age'/>
               <br/>
             </div>
 
             <div className='email' name='email'>
               <label>Enter Email</label>
               <br/>
-              <input ref={userName => this.userName = userName} type='email' placeholder ='Email'/>
+              <input ref={email => this.email = email} type='email'  onChange = { this.handle_Change }  placeholder ='Email'/>
               <br/>
             </div>
 
             <div className='password' name='password'>
               <label>Enter Password</label>
               <br/>
-              <input ref={userPass => this.userPass = userPass} type='text' placeholder ='Password'/>
+              <input ref={userPass => this.userPass = userPass} type='text'  onChange = { this.handle_Change }  placeholder ='Password'/>
               <br/>
                 <input type='submit' value='Submit'/>
             </div>
